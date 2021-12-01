cd52efde449ab13872c9517dd74417ff7a9c699c
yaeria <bonamymickael@yahoo.fr>
2021-12-01 16:50:52 +0100

    vespa animation

--- scripthistoire.js
+++ scripthistoire.js
@@ -33,3 +33,15 @@ function slidePrecedente(){
 
 
 precedent.addEventListener('click', slidePrecedente)
+
+var anim = document.querySelectorAll('.display-anim');
+document.getElementById('play_stop').onclick = function () {
+    for (var i = 0; i < anim.length; i++) {
+        if (anim[i].style.animationPlayState == 'paused') {
+            anim[i].style.animationPlayState = 'running';
+        }
+        else {
+            anim[i].style.animationPlayState = 'paused';
+        }
+    }
+}
