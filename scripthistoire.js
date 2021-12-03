17b31aeb3ba99e8ce1bbf2d6347d7e77f431e058
yaeria <bonamymickael@yahoo.fr>
2021-12-03 14:10:41 +0100

    maj header footer

--- scripthistoire.js
+++ scripthistoire.js
@@ -1,47 +1,57 @@
-const items = document.querySelectorAll('img');
-const nbSlide = items.length;
+const items = document.querySelectorAll('.image');
+
+/* On stock le nombre d'image  -1 car en js on compte a partir de Zero */
+const nbSlide = items.length ; 
+
+/* Recup des boutons en javascript */
 const suivant = document.querySelector('.droite');
 const precedent = document.querySelector('.gauche');
+
+/* Variable de comptage */
 let count = 0;
 
-function slideSuivante(){
-    items[count].classList.remove('active');
 
-    if(count < nbSlide - 1){
+/* Event des boutons */
+suivant.addEventListener('click', slideSuivante)
+precedent.addEventListener('click', slidePrecedente)
+
+
+/* Regle le bug du premier clic */
+suivant.click()
+
+
+/* Function slide suivante */
+
+
+function slideSuivante(){
+    
+    if(count < nbSlide){
+        items[count].classList.remove('active');
         count++;
+        items[count].classList.add('active')
     }else{
+        items[count].classList.remove('active');
         count = 0;
+        items[0].classList.add('active')
     }
 
-    items[count].classList.add('active')
-    console.log(count);
 }
 
-suivant.addEventListener('click', slideSuivante)
+/* Function slide precedente */
 
 
 function slidePrecedente(){
-    items[count].classList.remove('active')
-    if(count >0){
+
+    
+    if(count > 0){
+        items[count].classList.remove('active')
         count--;
+        items[count].classList.add('active')
     }
     else{
-        count =nbSlide -1;
+        items[count].classList.remove('active')
+        count = nbSlide ;
+        items[0].classList.add('active')
     }
-    items[count].classList.add('active')    
-}
 
-
-precedent.addEventListener('click', slidePrecedente)
-
-var anim = document.querySelectorAll('.display-anim');
-document.getElementById('play_stop').onclick = function () {
-    for (var i = 0; i < anim.length; i++) {
-        if (anim[i].style.animationPlayState == 'paused') {
-            anim[i].style.animationPlayState = 'running';
-        }
-        else {
-            anim[i].style.animationPlayState = 'paused';
-        }
-    }
 }
